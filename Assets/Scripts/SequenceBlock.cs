a1b61c79d38693af2902265dff0331413407048d
owengibson <114120616+owengibson@users.noreply.github.com>
2023-08-17 18:57:11 +1000

    Added audio source to sequence block prefab

--- Assets/Scripts/SequenceBlock.cs
+++ Assets/Scripts/SequenceBlock.cs
@@ -6,29 +6,20 @@ namespace GPG212_09
 {
     public class SequenceBlock : MonoBehaviour
     {
+        private AudioSource _audio;
+
+        private void Start()
+        {
+            _audio = GetComponent<AudioSource>();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             Debug.Log("Colliding with " + other.name);
             if (!other.CompareTag("Player")) return;
 
+            _audio.Play();
             EventManager.onSequenceBlockStep?.Invoke(this);
         }
-
-        /*private void OnCollisionEnter(Collision collision)
-        {
-            if (!collision.gameObject.CompareTag("Player")) return;
-
-            Debug.Log("Colliding with " + collision.gameObject.name);
-            EventManager.onSequenceBlockStep?.Invoke(gameObject);
-
-        }*/
-
-        /*private void OnControllerColliderHit(ControllerColliderHit hit)
-        {
-            Debug.Log("Colliding with " + hit.gameObject.name);
-            if (!hit.gameObject.CompareTag("SequenceBlock")) return;
-
-            EventManager.onSequenceBlockStep?.Invoke(hit.gameObject);
-        }*/
     }
 }
