1688bc3e147274edad10798810210ccb597925e4
Owen Gibson <114120616+owengibson@users.noreply.github.com>
2023-08-18 13:13:32 +1000

    Small bug fixes

---
+++ Assets/Scripts/MenuScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GPG212_09
+{
+    public class MenuScene : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
--- Assets/_THIRD PARTY/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/_THIRD PARTY/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -108,6 +108,8 @@ namespace StarterAssets
 			// reset our timeouts on start
 			_jumpTimeoutDelta = JumpTimeout;
 			_fallTimeoutDelta = FallTimeout;
+
+			Cursor.visible = false;
 		}
 
 		private void Update()
